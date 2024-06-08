3a34e51d1aaa45d04dcb0c371805d1864e0c667f
Sergey Korobeynikov <skoro@smn35.com>
2024-06-08 14:57:38 +0300

    add get_data to widget.py

--- src/date.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def get_data(date_str: str) -> str:
-    """Функция, возвращающая строку с датой в формате DD.MM.YYYY """
-    no_format_date = datetime.strptime(date_str[:10], ("%Y-%m-%d"))
-    format_date = no_format_date.strftime("%d.%m.%Y")
-    print(no_format_date)
-    return format_date
--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any
 
 import masks
@@ -11,3 +12,13 @@ def mask_account_card(string: str) -> Any:
     else:
         cardnumber = "".join(string[-16:].split())
         return string[:-16] + masks.mask_card_number(cardnumber)
+
+
+
+
+def get_data(date_str: str) -> str:
+    """Функция, возвращающая строку с датой в формате DD.MM.YYYY """
+    no_format_date = datetime.strptime(date_str[:10], ("%Y-%m-%d"))
+    format_date = no_format_date.strftime("%d.%m.%Y")
+    print(no_format_date)
+    return format_date
