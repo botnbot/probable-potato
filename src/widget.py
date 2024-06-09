15378998db36bcbf6ea4a0a7a0b0b0b4d006a0b5
Sergey Korobeynikov <skoro@smn35.com>
2024-06-09 10:01:30 +0300

    small widget fixies

--- src/widget.py
+++ src/widget.py
@@ -20,5 +20,4 @@ def get_data(date_str: str) -> str:
     """Функция, возвращающая строку с датой в формате DD.MM.YYYY """
     no_format_date = datetime.strptime(date_str[:10], ("%Y-%m-%d"))
     format_date = no_format_date.strftime("%d.%m.%Y")
-    print(no_format_date)
     return format_date
