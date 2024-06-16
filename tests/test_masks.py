dca22f12df4c21cbc0d722ae50b9316bea3f0a74
Sergey Korobeynikov <skoro@smn35.com>
2024-06-16 16:59:44 +0300

    small fixture fix

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,13 +5,13 @@ from src.masks import mask_card_number, mask_account
 
 
 @pytest.fixture
-def my_string():
+def number():
     return '1234567812345678'
 
 
-def test_mask_card_number(my_string):
-    assert mask_card_number(my_string) == '1234 56** **** 5678'
+def test_mask_card_number(number):
+    assert mask_card_number(number) == '1234 56** **** 5678'
 
 
-def test_mask_account(my_string):
-    assert mask_account(my_string) == '**5678'
+def test_mask_account(number):
+    assert mask_account(number) == '**5678'
