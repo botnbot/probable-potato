c882f6b53e364f1f3f4e660bf081df513c130249
Sergey Korobeynikov <skoro@smn35.com>
2024-06-20 16:30:28 +0300

    generators module fix

--- src/generators.py
+++ src/generators.py
@@ -1,20 +1,22 @@
-# card_number_generator
-def cardnumber_generator(start, end):
-    for number in range(start, end + 1):
-        yield f"{number:016d}".replace("", " ").strip()
+from typing import Generator
 
-def transaction_descriptions(transactions):
+
+def cardnumber_generator(start: int, end: int) -> Generator:
+    """Генератор номеров банковских карт"""
+    for number in range(start, end + 1):
+        cardnumber_str = f"{number:016d}"
+        formatted_card_number = ' '.join(cardnumber_str[i:i + 4] for i in range(0, 16, 4))
+        yield formatted_card_number
+
+
+def transaction_descriptions(transactions: list) -> Generator:
+    """Функция которая выдает описание каждой операции по очереди."""
     for transaction in transactions:
         yield transaction["description"]
 
 
-def filter_by_currency(transactions, currency):
+def filter_by_currency(transactions: list, currency: str) -> Generator:
+    """Функция которая выдает по очереди операции, в которых указана заданная валюта."""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
-
-
-# Usage of card_number_generator
-# print("\nCard Number Generator:")
-# for cardnumber in cardnumber_generator(99999999799, 99999999999):
-#     print(cardnumber)
