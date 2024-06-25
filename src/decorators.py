1c10c17d950c2aa5ca1059dfc6945f6d3d0f7de1
Sergey Korobeynikov <skoro@smn35.com>
2024-06-25 12:18:06 +0300

    test_decorators fix    README.md fix

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+from src.config import ROOT_PATH
+from pathlib import Path
+
+
+def log(filename=None):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(Path(ROOT_PATH, filename), "a") as log_file:
+                        log_file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(Path(ROOT_PATH, filename), "a") as log_file:
+                        log_file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
+                else:
+                    print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n")
+                raise e
+
+        return wrapper
+
+    return decorator
+
---
+++ tests/test_decorators.py
@@ -0,0 +1,31 @@
+import pytest
+from src.decorators import log
+from src.config import ROOT_PATH
+from pathlib import Path
+
+
+@log(filename="../src/log_file.txt")
+def divide(a, b):
+    '''Тстовая функция, которую будем использовать в тестах декоратора'''
+    return a / b
+
+
+def test_log_successful():
+    '''Проверяем, что лог записывается при успешном выполнении функции'''
+    log_file = Path(ROOT_PATH, "../src/log_file.txt")
+    result = divide(4, 2)
+    assert result == 2.0
+    with open(log_file, "r") as f:
+        readable = f.read()
+        assert "divide ok" in readable
+
+
+def test_log_err():
+    '''Проверяем, что лог записывается при возникновении исключения'''
+    log_file = Path(ROOT_PATH, "../src/log_file.txt")
+    with pytest.raises(TypeError):
+        divide(1, "2")
+    assert log_file.exists()
+    with open(log_file, 'r') as f:
+        log_contents = f.read()
+        assert "divide error: unsupported operand type(s) for /: 'int' and 'str'. Inputs: (1, '2'), {}" in log_contents
