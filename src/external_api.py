49eea063351db8cbc9cdd3ed37868899a64cd3d0
Sergey Korobeynikov <skoro@smn35.com>
2024-07-03 17:16:11 +0300

    external_api.py module added in src

---
+++ src/external_api.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+apikey = os.getenv("API_KEY")
+# print(apikey)
+transaction = {
+    "id": 550607912,
+    "state": "EXECUTED",
+    "date": "2018-07-31T12:25:32.579413",
+    "operationAmount": {"amount": "10", "currency": {"name": "USD", "code": "USD"}},
+    "description": "Перевод организации",
+    "from": "MasterCard 8532498887072395",
+    "to": "Счет 44238164562083919420",
+}
+
+
+def convert_to_rub(transaction: dict) -> float:
+    amount = transaction["operationAmount"]["amount"]
+    currency = transaction["operationAmount"]["currency"]["code"]
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+    headers = {"apikey": apikey}
+    response = requests.request("GET", url, headers=headers)
+
+    return response.json()["result"]
+
+
+print(convert_to_rub(transaction))
