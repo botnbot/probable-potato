b8d7caf7c65fb790c4ffd2687ec8cbfabb003da8
Sergey Korobeynikov <skoro@smn35.com>
2024-07-04 14:57:52 +0300

    test_utils.py module added in tests

--- src/external_api.py
+++ src/external_api.py
@@ -1,23 +1,24 @@
 import os
+from typing import Any
+
 import requests
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
 apikey = os.getenv("API_KEY")
-# print(apikey)
-transaction = {
-    "id": 550607912,
-    "state": "EXECUTED",
-    "date": "2018-07-31T12:25:32.579413",
-    "operationAmount": {"amount": "10", "currency": {"name": "USD", "code": "USD"}},
-    "description": "Перевод организации",
-    "from": "MasterCard 8532498887072395",
-    "to": "Счет 44238164562083919420",
-}
+# transaction = {
+#     "id": 550607912,
+#     "state": "EXECUTED",
+#     "date": "2018-07-31T12:25:32.579413",
+#     "operationAmount": {"amount": "10", "currency": {"name": "USD", "code": "USD"}},
+#     "description": "Перевод организации",
+#     "from": "MasterCard 8532498887072395",
+#     "to": "Счет 44238164562083919420",
+# }
 
 
-def convert_to_rub(transaction: dict) -> float:
+def convert_to_rub(transaction: dict) -> Any:
     amount = transaction["operationAmount"]["amount"]
     currency = transaction["operationAmount"]["currency"]["code"]
 
@@ -26,6 +27,3 @@ def convert_to_rub(transaction: dict) -> float:
     response = requests.request("GET", url, headers=headers)
 
     return response.json()["result"]
-
-
-print(convert_to_rub(transaction))
