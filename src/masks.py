e16aaf604042ddeec9f718c59f492d0850df991a
Sergey Korobeynikov <skoro@smn35.com>
2024-07-09 17:02:02 +0300

    xlsx&csv files converting added in utils

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 import logging
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/masks.log', mode='w')
-file_formater = logging.Formatter('%(asctime)s -%(name)s -%(levelname)s: %(message)s')
+file_handler = logging.FileHandler("logs/masks.log", mode="w")
+file_formater = logging.Formatter("%(asctime)s -%(name)s -%(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
 
@@ -11,32 +11,27 @@ logger.addHandler(file_handler)
 def mask_card_number(cardnumber: str) -> str:
     """Функция, маскирующая номер карты"""
     try:
-        logger.info('Маскировка номера карты')
+        logger.info("Маскировка номера карты")
         masked_cardnumber_list = []
         masked_cardnumber = cardnumber[:-10] + "******" + cardnumber[-4:]
 
-
         for i in range(0, len(masked_cardnumber), 4):
-            masked_cardnumber_list.append(masked_cardnumber[i: i + 4])
-
+            masked_cardnumber_list.append(masked_cardnumber[i : i + 4])
 
         result = " ".join(masked_cardnumber_list)
-        logger.info('Номер карты успешно замаскирован')
+        logger.info("Номер карты успешно замаскирован")
         return result
 
     except Exception as e:
-        logger.error(f'Ошибка при маскировке номера карты: {e}')
+        logger.error(f"Ошибка при маскировке номера карты: {e}")
+
 
 def mask_account(account: str) -> str:
     """Функция, маскирующая номер счета"""
     try:
-        logger.info('Маскировка номера счета')
+        logger.info("Маскировка номера счета")
         result = "**" + account[-4:]
-        logger.info('Номер счета успешно замаскирован')
+        logger.info("Номер счета успешно замаскирован")
         return result
     except Exception as e:
-        logger.error(f'Ошибка при маскировке номера счета: {e}')
-
-
-
-
+        logger.error(f"Ошибка при маскировке номера счета: {e}")
--- src/utils.py
+++ src/utils.py
@@ -1,48 +1,62 @@
 import json
-
 import logging
 from pathlib import Path
 from typing import Any, Dict, List
 
-logger = logging.getLogger('utils')
+import pandas as pd
+
+# Настройка логирования
+logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs/utils.log', mode='w')
-file_formater = logging.Formatter('%(asctime)s -%(name)s -%(levelname)s: %(message)s')
-file_handler.setFormatter(file_formater)
+file_handler = logging.FileHandler("logs/utils.log", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def json_to_list(path_to_file: str) -> List[Dict[str, Any]]:
-    """Функция преобразования JSON файла в список словарей с данными о транзакциях."""
+def file_to_list(path_to_file: str) -> List[Dict[str, Any]]:
+    """Функция преобразования файлов с данными о транзакциях в список словарей.
+
+    Поддерживаемые форматы: JSON, XLSX, CSV.
+    """
     file_path = Path(path_to_file)
     transactions = []
 
     try:
         if not file_path.is_file():
-
             logger.error(f"Файл '{file_path}' не найден.")
             raise FileNotFoundError(f"Файл '{file_path}' не найден.")
 
-        logger.info('Чтение JSON-файла')
+        file_extension = file_path.suffix.lower()
 
-        with open(file_path, encoding="utf-8") as f:
-            data = json.load(f)
+        if file_extension == ".json":
+            logger.info("Чтение JSON-файла")
+            with open(file_path, encoding="utf-8") as f:
+                data = json.load(f)
+            if not isinstance(data, list):
+                logger.error(f"JSON-фaйл '{file_path}' содержит не список")
+                raise ValueError(f"JSON-фaйл '{file_path}' содержит не список")
+            for item in data:
+                if not isinstance(item, dict):
+                    logger.error(f"JSON-фaйл '{file_path}' содержит элементы, которые не являются словарями")
+                    raise ValueError(f"JSON-фaйл '{file_path}' содержит элементы, которые не являются словарями")
+            transactions = data
 
-        if not isinstance(data, list):
+        elif file_extension == ".xlsx":
+            logger.info("Чтение XLSX-файла")
+            excel_df = pd.read_excel(file_path)
+            transactions = excel_df.to_dict(orient="records")
 
-            logger.error(f"Файл '{file_path}' содержит не список")
-            raise ValueError(f"Файл '{file_path}' содержит не список")
-            transactions = []
-        for item in data:
+        elif file_extension == ".csv":
+            logger.info("Чтение CSV-файла")
+            csv_df = pd.read_csv(file_path)
+            transactions = csv_df.to_dict(orient="records")
 
-            if not isinstance(item, dict):
-                logger.error(f"Файл '{file_path}' содержит элементы, которые не являются словарями")
+        else:
+            logger.error("Неподдерживаемый тип файла")
+            raise ValueError("Неподдерживаемый тип файла")
 
-                raise ValueError(f"Файл '{file_path}' содержит элементы, которые не являются словарями")
-
-        transactions = data
-
-    except FileNotFoundError:0,
+    except FileNotFoundError:
         logger.error(f"Файл '{file_path}' не найден.")
         print(f"Файл '{file_path}' не найден.")
     except ValueError as e:
@@ -51,6 +65,6 @@ def json_to_list(path_to_file: str) -> List[Dict[str, Any]]:
     except Exception as e:
         logger.error(f"Произошла ошибка при чтении файла '{file_path}': {str(e)}")
         print(f"Произошла ошибка при чтении файла '{file_path}': {str(e)}")
-    logger.info('Вывод  результата')
-    return transactions
 
+    logger.info("Вывод результата")
+    return transactions
