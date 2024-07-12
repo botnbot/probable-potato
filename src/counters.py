61363bf81cf9c128cb7b135c526f02554e39ee1b
Sergey Korobeynikov <skoro@smn35.com>
2024-07-13 00:52:47 +0300

    counters module added search_string_in_transactions func in processing module added

---
+++ src/counters.py
@@ -0,0 +1,10 @@
+from typing import List, Any, Dict
+def search_by_category(transactions: List[Dict[str, Any]], categories: List[str]) -> dict:
+    '''Функция, возвращающая словарь с количеством операций по категориям'''
+    category_count = {category: 0 for category in categories}
+    for transaction in transactions:
+      description = transaction['description']
+      for category in categories:
+        if category in description:
+          category_count[category] += 1
+    return category_count
--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,33 @@
-from typing import Iterable
+from typing import List, Any, Dict, Iterable
+import re
+
+def filter_by_currency(transactions: list, currency: str) -> List[dict]:
+    """Функция которая выдает список транзакций, в которых указана заданная валюта."""
+    filtered_by_currency = []
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+             filtered_by_currency.append(transaction)
+
+    return filtered_by_currency
 
 
 def filter_by_state(list_of_operations: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """Функция фильтрации по статусу (state) операции"""
-    list_of_selected_operations = []
+    """Функция фильтрации транзакций по статусу (state) операции"""
+    filtered_by_state = []
     for operation in list_of_operations:
         if operation.get("state") == state:
-            list_of_selected_operations.append(operation)
+            filtered_by_state.append(operation)
 
-    return list_of_selected_operations
+    return filtered_by_state
+
+def search_string_in_transactions(transactions: List[Dict[str, Any]], string_to_find: str) -> List[Dict[str, Any]]:
+    '''Функция фильтрации транзакций по строке в описании'''
+    list_of_selected_transactions =[]
+    for transaction in transactions:
+        if re.search(string_to_find, transaction["description"],flags=re.IGNORECASE) :
+            list_of_selected_transactions.append(transaction)
+
+    return list_of_selected_transactions
 
 
 def sort_by_date(list_of_operations: Iterable, ascending: bool = True) -> list[dict]:
