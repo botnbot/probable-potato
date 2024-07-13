3822fcb6b291f478f41a605167b8936682a84f0f
Sergey Korobeynikov <skoro@smn35.com>
2024-07-13 20:18:24 +0300

    widget module fixed

--- src/widget.py
+++ src/widget.py
@@ -6,6 +6,8 @@ from src.masks import mask_account, mask_card_number
 
 def mask_account_card(string: str) -> Any:
     """Функция, маскирующая счета и карты"""
+    if not isinstance(string, str):
+        return string
     if "Счет " in string:
         account = string[-20:]
         return string[:-20] + mask_account(account)
@@ -19,3 +21,6 @@ def get_data(date_str: str) -> str:
     no_format_date = datetime.strptime(date_str[:10], ("%Y-%m-%d"))
     format_date = no_format_date.strftime("%d.%m.%Y")
     return format_date
+
+
+print(mask_account_card("Счет 58803664561298323391"))
