44d8a51ffa36ba2ad23233b30965e73b42e50e81
Sergey Korobeynikov <skoro@smn35.com>
2024-07-13 23:48:39 +0300

    counters.py correcting (search_by_category func)

--- src/counters.py
+++ src/counters.py
@@ -1,12 +1,11 @@
 from typing import List, Any, Dict
+from collections import Counter
 
 
 def search_by_category(transactions: List[Dict[str, Any]], categories: List[str]) -> dict:
     """Функция, возвращающая словарь с количеством операций по категориям"""
-    category_count = {category: 0 for category in categories}
+    category_count = Counter()
     for transaction in transactions:
-        description = transaction["description"]
-        for category in categories:
-            if category in description:
-                category_count[category] += 1
-    return category_count
+        if transaction["state"] in categories:
+            category_count[transaction['state']] += 1
+    return dict(category_count)
